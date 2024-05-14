b47b33693212f7cb1586ef6895be36607a4535e4
unknown <mcahya830@gmail.com>
2024-05-14 09:42:02 +0700

    FINAL MODUL 4

--- src/TextBook.java
+++
@@ -1,7 +0,0 @@
-public class TextBook extends Book{
-    private String category = "TextBook";
-    TextBook(String bookId, String title, String author, int stock){
-        super(bookId,title,author,stock);
-        super.setCategory(category);
-    }
-}
